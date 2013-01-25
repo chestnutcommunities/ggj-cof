8e515ad62227989480245da818793bfd0c137f8e
Shingo Tamura <shingokko@gmail.com>
2013-01-25 14:31:04 +1300

    Add positioning helper

--- ggj-cof/PositioningHelper.m
+++ ggj-cof/PositioningHelper.m
@@ -10,4 +10,89 @@
 
 @implementation PositioningHelper
 
++(CGPoint)getViewpointPosition:(CGPoint)position {
+    CGSize winSize = [[CCDirector sharedDirector] winSize];
+    
+    // We want the player to be always at the centre of the screen so
+    // just use the position that's been passed here
+    int x = position.x;
+    int y = position.y;
+    
+    CGPoint actualPosition = ccp(x, y);
+    CGPoint centerOfView = ccp(winSize.width/2, winSize.height/2);
+    
+    return ccpSub(centerOfView, actualPosition);
+}
+
++(CGPoint)convertPixelsToPoints:(CGPoint)pixels retina:(BOOL)retina {
+    if (retina) {
+        return ccp(pixels.x / 2.0f, pixels.y / 2.0f);
+    }
+    else {
+        return ccp(pixels.x, pixels.y);
+    }
+}
+
++(CGPoint)convertPointsToPixels:(CGPoint)points retina:(BOOL)retina {
+    if (retina) {
+        return ccp(points.x * 2.0f, points.y * 2.0f);
+    }
+    else {
+        return ccp(points.x, points.y);
+    }
+}
+
+// Gets tile coordinates for the given position in pixels. Use actual tile size
++(CGPoint)tileCoordForPositionInPixels:(CGPoint)positionInPixels tileMap:(CCTMXTiledMap*)tileMap {
+    // Tile coordinates in int
+    int x = positionInPixels.x / tileMap.tileSize.width;
+    int y = ((tileMap.mapSize.height * tileMap.tileSize.height) - positionInPixels.y) / tileMap.tileSize.height;
+    
+    return ccp(x, y);
+}
+
+// Gets tile coordinates for the given position in points. The position must be
+// in points.
++(CGPoint)tileCoordForPositionInPoints:(CGPoint)positionInPoints tileMap:(CCTMXTiledMap*)tileMap tileSizeInPoints:(CGSize)tileSizeInPoints {
+    // Tile coordinates in int
+    int x = positionInPoints.x / tileSizeInPoints.width;
+    int y = ((tileMap.mapSize.height * tileSizeInPoints.height) - positionInPoints.y) / tileSizeInPoints.height;
+    
+    return ccp(x, y);
+}
+
++(CGPoint)positionInPixelsForTileCoord:(CGPoint)tileCoord tileMap:(CCTMXTiledMap*)tileMap {
+    CGFloat x = (tileCoord.x * tileMap.tileSize.width) + tileMap.tileSize.width / 2.0f;
+    CGFloat y = (tileMap.mapSize.height * tileMap.tileSize.height) - (tileCoord.y * tileMap.tileSize.height) - tileMap.tileSize.height / 2.0f;
+    
+    return ccp(x, y);
+}
+
++(CGPoint)positionInPointsForTileCoord:(CGPoint)tileCoord tileMap:(CCTMXTiledMap*)tileMap tileSizeInPoints:(CGSize)tileSizeInPoints {
+    CGFloat x = (tileCoord.x * tileSizeInPoints.width) + tileSizeInPoints.width / 2.0f;
+    CGFloat y = (tileMap.mapSize.height * tileSizeInPoints.height) - (tileCoord.y * tileSizeInPoints.height) - tileSizeInPoints.height / 2.0f;
+    
+    return ccp(x, y);
+}
+
+// Compute a position (in points) that fits to the corresponding tile
++(CGPoint) computeTileFittingPositionInPoints:(CGPoint)positionInPoints tileMap:(CCTMXTiledMap*)tileMap tileSizeInPoints:(CGSize)tileSizeInPoints {
+    CGPoint tilePos = [self tileCoordForPositionInPoints:positionInPoints tileMap:tileMap tileSizeInPoints:tileSizeInPoints];
+    
+    CGFloat x = (tilePos.x * tileSizeInPoints.width) + (tileSizeInPoints.width / 2.0f);
+    CGFloat y = (tileMap.mapSize.height * tileSizeInPoints.height) - (tilePos.y * tileSizeInPoints.height) - (tileSizeInPoints.height / 2.0f);
+    
+    return ccp(x, y);
+}
+
+// Compute a position (in pixels) that fits to the corresponding tile
++(CGPoint) computeTileFittingPositionInPixels:(CGPoint)positionInPixels tileMap:(CCTMXTiledMap*)tileMap tileSizeInPoints:(CGSize)tileSizeInPoints {
+    CGPoint tilePos = [self tileCoordForPositionInPixels:positionInPixels tileMap:tileMap];
+    
+    CGFloat x = (tilePos.x * tileMap.tileSize.width) + (tileMap.tileSize.width / 2.0f);
+    CGFloat y = (tileMap.mapSize.height * tileMap.tileSize.height) - (tilePos.y * tileMap.tileSize.height) - (tileMap.tileSize.height / 2.0f);
+    
+    return ccp(x, y);
+}
+
 @end
